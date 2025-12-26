3e571ac1fbd578254b42cf0fe537c98fbb565778
jayshritamboli <jayshri.ptamboli@gmail.com>
2025-12-26 22:31:02 +0530

    changes in html

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,6 +8,7 @@ import { Itodo } from '../models/todo';
 })
 export class TodoComponent implements OnInit {
 
+
 todoArr : Array<Itodo> = [
   {
     todoItem:"JavaScript",
@@ -36,33 +37,68 @@ todoArr : Array<Itodo> = [
   }
 ]
 
+  editId !: string;
+  isEditMode : boolean = false;
   @ViewChild('todoItem') todoItem !: ElementRef
 
   ngOnInit(): void {
   }
 
   onTodoAdd(){
+    //check if input field is not empty
+    if( this.todoItem.nativeElement.value.length > 0){
+      //create TODO Obj
     let todoObj : Itodo = {
       todoItem : this.todoItem.nativeElement.value,
       todoId : Date.now().toString()
     }
-
+    //clear input field
     this.todoItem.nativeElement.value = ''
     console.log(todoObj);
-
+    //push into array
     this.todoArr.push(todoObj);
-
   }
-
+  }
+  //trackBy function
+  //helps angular to identify each item uniquely
   trackById (index: number, todo: Itodo){
     return todo.todoId
   }
 
+
+//remove todo
   onRemove(id:string){
     console.log(id);
-
+    //get index of todo to be removed
     let getIndex = this.todoArr.findIndex(t => t.todoId === id);
     this.todoArr.splice(getIndex,1)
   }
 
+
+  onEdit(todo: Itodo){
+    console.log(todo);
+    //store the id of todo to be edited
+    this.editId=todo.todoId;
+    //set the value in input field
+    this.todoItem.nativeElement.value = todo.todoItem;
+    //Edit mode activated
+    this.isEditMode = true;
+  }
+
+
+  onUpdateTodo(){
+    let updated_Todo : Itodo = {
+      todoItem : this.todoItem.nativeElement.value,
+      todoId : this.editId
+    }
+    console.log(updated_Todo);
+    //clear input field
+    this.todoItem.nativeElement.value = '';
+    //get index of todo to be updated
+    let getIndex = this.todoArr.findIndex(t => t.todoId ===updated_Todo.todoId);
+    //update the todo in array
+    this.todoArr[getIndex] = updated_Todo;
+    //exit edit mode
+    this.isEditMode = false;
+  }
 }
